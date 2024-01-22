cfc44b5658123a935ceda5e11e8f25c5d9b31d47
Jean Loberg Hansen <jlh@kruso.dk>
2024-01-22 15:18:25 +0100

    update at work on aside.

--- app/ui/section/aside/aside.tsx
+++ app/ui/section/aside/aside.tsx
@@ -8,16 +8,19 @@ export default async function Aside({ data }: CMS.IAside) {
   return (
     <aside className={style.aside}>
       <div className={style.aside__contactBox}>
-        <div>
-          <span>icon</span>
-          <a href={"tel:" + data.phonenumber}>{data.phonenumber}</a>
+        <div className={style.aside__icon}>
+          <AtomImage src="/phone-icon.svg" alt="kontakt mig på telefon" width={16} height={16} />
         </div>
+        <a href={"tel:" + data.phonenumber}>{data.phonenumber}</a>
+
         <div>
           <span>icon</span>
           <a href={"mailto:" + data.email}>{data.email}</a>
         </div>
       </div>
-      <AtomImage src="/gv.png" alt="vinduespudser" width={1200} height={1000} />
+      <div className={style.aside__bgImage}>
+      <AtomImage  src="/gv.png" alt="vinduespudser" width={1200} height={1000} />
+      </div>
     </aside>
   );
 }
