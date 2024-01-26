5822e82d56674025b6ee0821ef85f6ead703c6ff
Jean Loberg Hansen <jlh@kruso.dk>
2024-01-26 10:55:12 +0100

    robots

---
+++ app/robots.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const allow = {
+    rules: [
+      { userAgent: "GPTBot", disallow: "/" },
+      { userAgent: "ChatGPT-user", disallow: "/" },
+      { userAgent: "*", disallow: " " },
+    ],
+  };
+
+  const disAllow = {
+    rules: [
+      { userAgent: "GPTBot", disallow: "/" },
+      { userAgent: "ChatGPT-user", disallow: "/" },
+      { userAgent: "*", disallow: "/" },
+    ],
+  };
+  const { IS_PROD } = process.env;
+
+  return IS_PROD === "gladvinduer" ? allow : disAllow;
+}
